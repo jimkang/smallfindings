3897ab421f0fa9fcf9e678d0951153a3b7fd6353
Jim Kang <jimkang@gmail.com>
2020-04-09 00:53:28 -0400

    A little louder.

--- src-audio/episode-2/prepare-audio.sh
+++ src-audio/episode-2/prepare-audio.sh
@@ -9,7 +9,7 @@
     stereofile="${stereodir}/${filename}"
     outfile="${outdir}/${filename}"
     echo "Converting to stereo and adding reverb: ${stereofile}."
-    sox -M "${file}" "${file}" "${stereofile}" gain -2 reverb 10 50 100 100
+    sox -M "${file}" "${file}" "${stereofile}" gain -1 reverb 10 50 100 100
     echo "Converting to 44.1 kHz sample rate and normalizing"
     sox "${stereofile}" --norm=-1 -r 44100 "${outfile}"
   done
