14c643537ac934b7ec4bcc3c9a8ac11857bf2815
Jim Kang <jimkang@gmail.com>
2020-04-10 21:36:33 -0400

    Try less reverb for next time.

--- prepare-audio.sh
+++ prepare-audio.sh
@@ -10,7 +10,7 @@
     stereofile="${stereodir}/${filename}"
     outfile="${outdir}/${filename}"
     echo "Converting to stereo and adding reverb: ${stereofile}."
-    sox -M "${file}" "${file}" "${stereofile}" gain -1 reverb 5 50 100 100
+    sox -M "${file}" "${file}" "${stereofile}" gain -1 reverb 2 50 100 100
     echo "Converting to 44.1 kHz sample rate and normalizing"
     sox "${stereofile}" --norm=-1 -r 44100 "${outfile}"
   done
