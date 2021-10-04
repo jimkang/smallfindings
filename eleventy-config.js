20c9ddace7258446a7862f92b66d8a3375797759
Jim Kang <jimkang@gmail.com>
2021-10-03 21:23:55 -0400

    Update RSS plugin. Implement a version of Dan's lastBuild fix in rehydrate.

--- eleventy-config.js
+++ eleventy-config.js
@@ -2,26 +2,25 @@ var pluginRss = require('@11ty/eleventy-plugin-rss');
 var fs = require('fs');
 var yaml = require('yamljs');
 
-module.exports = function(eleventyConfig) {
+module.exports = function (eleventyConfig) {
   // This makes `make serve` serve the site at localhost:8080/smallfindings
   eleventyConfig.setBrowserSyncConfig({
-    server: '.'
+    server: '.',
   });
   eleventyConfig.addPassthroughCopy('media/*');
   eleventyConfig.addPassthroughCopy('app.css');
   eleventyConfig.addPassthroughCopy('episodes/**/*.mp3');
 
-  eleventyConfig.addCollection('episodes', function(collection) {
+  eleventyConfig.addCollection('episodes', function (collection) {
     var filteredCollection = collection
       .getFilteredByGlob(['episodes/*.njk'])
       .sort(compareDatesDesc);
-    console.log('filteredCollection', filteredCollection);
     return filteredCollection;
   });
 
   eleventyConfig.addPlugin(pluginRss);
   eleventyConfig.addFilter('length', getFileLength);
-  eleventyConfig.addDataExtension('yaml', contents => yaml.parse(contents));
+  eleventyConfig.addDataExtension('yaml', (contents) => yaml.parse(contents));
 };
 
 function compareDatesDesc(a, b) {
